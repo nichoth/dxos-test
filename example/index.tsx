5135a8f42ac094d8f270deaeb1854fb1d9023569
nick thomas <nichoth@gmail.com>
2023-09-24 21:13:41 -0500

    wip

--- example/index.tsx
+++ example/index.tsx
@@ -1,4 +1,23 @@
+import { Client } from '@dxos/client';
 import { render } from 'preact'
+
+const client = new Client();
+
+(async () => {
+    await client.initialize()
+    let identity
+
+    try {
+        identity = client.halo.identity.get()
+    } catch (err) {
+        console.log('errrrrrrrrrrrrr', err)
+    }
+
+    // get a list of all spaces
+    // const spaces = client.spaces.get()
+
+    console.log('identity', identity)
+})()
 
 function Example () {
     return (<div>hello</div>)
